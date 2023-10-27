2096e9f4e420010bf5984dde848d13fa54bcb131
UnnamedOrange <lycheng1215@sina.com>
2023-10-27 11:16:07 +0800

    Update battery demo

--- src/battery/battery_info_windows.h
+++ src/battery/battery_info_windows.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
+#include <string_view>
+#include <vector>
 
 #include <Windows.h>
 #include <batclass.h>
@@ -12,58 +15,104 @@
 
 namespace orange::battery {
     inline void enumerate_batteries() noexcept {
-        // Enumerate battery devices.
-        HDEVINFO hDevInfo = SetupDiGetClassDevsW(&GUID_DEVCLASS_BATTERY, NULL, NULL, DIGCF_PRESENT);
-        if (hDevInfo == INVALID_HANDLE_VALUE) {
+        HDEVINFO hdevinfo = SetupDiGetClassDevsW(&GUID_DEVCLASS_BATTERY, //
+                                                 nullptr,                // Enumerator
+                                                 nullptr,                // hwndParent
+                                                 DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+        if (hdevinfo == INVALID_HANDLE_VALUE) {
             return;
         }
 
         // Query each battery device.
-        SP_DEVINFO_DATA devInfoData;
-        devInfoData.cbSize = sizeof(devInfoData);
-        for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &devInfoData); i++) {
-            // Get device path.
-            wchar_t devicePath[MAX_PATH];
-            if (!SetupDiGetDeviceRegistryPropertyW(hDevInfo,                            //
-                                                   &devInfoData,                        //
-                                                   SPDRP_PHYSICAL_DEVICE_OBJECT_NAME,   //
-                                                   nullptr,                             //
-                                                   reinterpret_cast<BYTE*>(devicePath), //
-                                                   sizeof(devicePath),                  //
-                                                   NULL)) {
-                continue;
+        for (auto idx = DWORD{0}; true; idx++) {
+            // Get interface data.
+            auto did = SP_DEVICE_INTERFACE_DATA{
+                .cbSize = sizeof(SP_DEVICE_INTERFACE_DATA),
+            };
+            {
+                const auto result = SetupDiEnumDeviceInterfaces(hdevinfo,               //
+                                                                nullptr,                // DeviceInfoData
+                                                                &GUID_DEVCLASS_BATTERY, //
+                                                                idx,                    //
+                                                                &did);
+                if (!result) {
+                    break; // Regarded as no more items.
+                }
             }
 
+            // Get device path.
+            DWORD required_size;
+            SetupDiGetDeviceInterfaceDetailW(hdevinfo, &did, nullptr, NULL, &required_size, nullptr);
+            auto buf = std::vector<std::byte>(required_size);
+            auto& didd = *reinterpret_cast<SP_DEVICE_INTERFACE_DETAIL_DATA_W*>(buf.data());
+            didd.cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA_W);
+            SetupDiGetDeviceInterfaceDetailW(hdevinfo, &did, &didd, required_size, &required_size, nullptr);
+
             // Open device.
-            HANDLE hDevice = CreateFileW(devicePath,                           //
-                                         GENERIC_READ | GENERIC_WRITE,         //
-                                         FILE_SHARE_READ | FILE_SHARE_WRITE,   //
-                                         nullptr,                              //
-                                         OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, //
-                                         nullptr);
-            if (hDevice == INVALID_HANDLE_VALUE) {
+            [[maybe_unused]] const auto _device_path = std::wstring_view{didd.DevicePath};
+            HANDLE hdev = CreateFileW(didd.DevicePath,                      //
+                                      GENERIC_READ | GENERIC_WRITE,         //
+                                      FILE_SHARE_READ | FILE_SHARE_WRITE,   //
+                                      nullptr,                              // dwCreationDisposition
+                                      OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, //
+                                      nullptr);                             // hTemplateFile
+            if (hdev == INVALID_HANDLE_VALUE) {
                 continue;
             }
 
             // Query battery information.
-            BATTERY_QUERY_INFORMATION batteryQueryInfo;
-            batteryQueryInfo.BatteryTag = 0;
-            batteryQueryInfo.InformationLevel = BatteryInformation;
-            DWORD bytesReturned;
-            if (!DeviceIoControl(hDevice, IOCTL_BATTERY_QUERY_INFORMATION, &batteryQueryInfo, sizeof(batteryQueryInfo),
-                                 nullptr, 0, &bytesReturned, NULL)) {
+            ULONG battery_tag;
+            auto timeout_ms = ULONG{0};
+            DWORD bytes_returned;
+            if (!DeviceIoControl(hdev,                    //
+                                 IOCTL_BATTERY_QUERY_TAG, //
+                                 &timeout_ms,             //
+                                 sizeof(timeout_ms),      //
+                                 &battery_tag,            //
+                                 sizeof(battery_tag),     //
+                                 &bytes_returned,         //
+                                 nullptr)) {              // lpOverlapped
                 goto FAIL_DEVICE_IO_CONTROL;
             }
-            BATTERY_INFORMATION batteryInfo;
-            if (!DeviceIoControl(hDevice, IOCTL_BATTERY_QUERY_INFORMATION, &batteryQueryInfo, sizeof(batteryQueryInfo),
-                                 &batteryInfo, sizeof(batteryInfo), &bytesReturned, NULL)) {
+            if (!battery_tag) {
+                goto FAIL_DEVICE_IO_CONTROL;
+            }
+
+            auto bqi = BATTERY_QUERY_INFORMATION{
+                .BatteryTag = battery_tag,
+            };
+            BATTERY_INFORMATION bi;
+            if (!DeviceIoControl(hdev,                            //
+                                 IOCTL_BATTERY_QUERY_INFORMATION, //
+                                 &bqi,                            //
+                                 sizeof(bqi),                     //
+                                 &bi,                             //
+                                 sizeof(bi),                      //
+                                 &bytes_returned,                 //
+                                 nullptr)) {                      // lpOverlapped
+                goto FAIL_DEVICE_IO_CONTROL;
+            }
+
+            auto bws = BATTERY_WAIT_STATUS{
+                .BatteryTag = battery_tag,
+                .Timeout = 0,
+            };
+            BATTERY_STATUS bs;
+            if (!DeviceIoControl(hdev,                       //
+                                 IOCTL_BATTERY_QUERY_STATUS, //
+                                 &bws,                       //
+                                 sizeof(bws),                //
+                                 &bs,                        //
+                                 sizeof(bs),                 //
+                                 &bytes_returned,            //
+                                 nullptr)) {                 // lpOverlapped
                 goto FAIL_DEVICE_IO_CONTROL;
             }
 
         FAIL_DEVICE_IO_CONTROL:
-            CloseHandle(hDevice);
+            CloseHandle(hdev);
         }
 
-        SetupDiDestroyDeviceInfoList(hDevInfo);
+        SetupDiDestroyDeviceInfoList(hdevinfo);
     }
 } // namespace orange::battery

9eb6f44df85d2088ecb9c94c55dc747d23bfaa54
UnnamedOrange <lycheng1215@sina.com>
2023-10-28 10:59:10 +0800

    Define types

--- src/battery/types.h
+++ src/battery/types.h
@@ -1,3 +1,24 @@
 #pragma once
 
-namespace orange::battery {}
+#include <cstdint>
+#include <string>
+
+namespace orange::battery {
+    struct BatteryInfo {
+        uint32_t capacity;
+        uint32_t power_state;
+        int32_t rate;
+        uint32_t voltage;
+
+        uint32_t designed_capacity;
+        uint32_t full_charged_capacity;
+        uint32_t default_alert_warning;
+        uint32_t default_alert_low;
+
+        std::string chemistry;
+        // uint8_t technology; // 1
+        // uint32_t capability; // 1 << 31
+        uint32_t critical_bias; // optional
+        uint32_t cycle_count;   // optional
+    };
+} // namespace orange::battery
